5e92721ab1a7361b4bcb2d013e557e1afd577afd
Jose <casanovajose@users.noreply.github.com>
2024-06-03 09:51:27 -0300

    Add files via upload

--- js/funciones.js
+++ js/funciones.js
@@ -1,34 +1,76 @@
-// console.log("Hola JS");
-// console.log(5);
-// console.log("5");
-// console.log(5 + 5);
-// console.log(5 + 5 + "5" + 5);
-// console.log("hola" - 5);
-/*
-fgfdknkjl
-hjdghod
-njkgd
-*/
+
+let video;
+let poseNet;
+let poses = [];
+let canvas = null;
+
+const grillaCapturas = document.querySelector(".grid");
+const cuerpo = ["leftWrist", "rightWrist", "nose"];
+
+let parteCuerpo = null;
+let posicionParteCuerpo = {x: -100, y: -100}
+let objetivo = null;
 
 function setup() {
-  console.log("Hola p5 js");
+  canvas = createCanvas(640, 480, document.querySelector("canvas"));
+  video = createCapture(VIDEO, { flipped:true });
+  video.size(width, height);
   rectMode(CENTER);
-  
-  let canvas = createCanvas(400, 400);
-  canvas.parent("anim");
+  objetivo = nuevoObjetivo();
+  parteCuerpo = elegirElemento(cuerpo);
 
-  background(100, 0, 0);
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, "single", function cargoModelo() {
+    console.log("Ya cargo ML5 posenet");
+  });
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
+    console.log(parteCuerpo);
+    poses = results[0];
+
+    if (poses.pose[parteCuerpo].confidence > 0.8 ) {
+      posicionParteCuerpo.x = width - poses.pose[parteCuerpo].x;
+      posicionParteCuerpo.y = poses.pose[parteCuerpo].y;
+    };
+  });
+  
+  video.hide();
+}
+
+function elegirElemento(lista) {
+  return lista[Math.floor(Math.random()*lista.length)];
+}
+
+function nuevoObjetivo() {
+  return {
+    x: random(50, width - 50),
+    y: random(50, height - 50)
+  }
 }
 
 function draw() {
-  // console.log("Nro de frame: " + frameCount);
-  // console.log("Hola estoy en el draw");
-  // 
+  image(video, 0, 0, width, height);
 
-  fill(0, 150, 0);
-  circle(mouseX, mouseY, 50);
-  // console.log("mouse x: " + mouseX + " mouse y: " + mouseY);
-  
-  fill(mouseY, mouseX, 150);
-  square(width/2, height/2, 400 - frameCount);
+  if (dist(objetivo.x, objetivo.y, posicionParteCuerpo.x, posicionParteCuerpo.y) < 20) { 
+    objetivo = nuevoObjetivo();
+    parteCuerpo = elegirElemento(cuerpo);
+    const captura = canvas.elt.toDataURL();
+    console.log(captura);
+    crearImagen(grillaCapturas, captura);
+  }
+
+  stroke(0, 255, 0);
+  fill(0, 255, 0);
+  circle(objetivo.x, objetivo.y, 10);
+  rect(posicionParteCuerpo.x, posicionParteCuerpo.y, 40, 40);
+}
+
+function crearImagen(grilla, src) {
+  const div = document.createElement("div");
+  div.className = "obra";
+  const img = document.createElement("img");
+  img.src = src;
+  div.append(img);
+  grilla.append(div);
 }
